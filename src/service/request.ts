0a2ca72795b28c969f85f3d16262429207c174a5
nan980914 <17691191896@163.com>
2021-01-21 22:46:08 +0800

    fix: https

--- src/service/request.ts
+++ src/service/request.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const queryCity = (cityid:number,type:string) => {
-    let url = `http://service-cj8nltie-1253369406.gz.apigw.tencentcs.com:80/http-proxy?cityId=${cityid}`
+    let url = `https://service-cj8nltie-1253369406.gz.apigw.tencentcs.com:443/http-proxy?cityId=${cityid}`
     return axios.get(url).then(res=>{
         return res.data
     })
